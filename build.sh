802a7c24b84e72906bc4d61308aa55fa6fd4be08
zemiak <zemiak@gmail.com>
2019-05-13 22:12:08 +0200

    build v1

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-export JEKYLL_VERSION=3.8
-docker rm -f jekyll-builder-${JEKYLL_VERSION}
-docker run -d jekyll/builder:$JEKYLL_VERSION //bin/true
-sleep 5s
-docker cp -r ./_src/* jekyll-builder-${JEKYLL_VERSION}:/srv/jekyll/
-docker exec jekyll-builder-${JEKYLL_VERSION} jekyll build
-docker rm -f jekyll-builder-${JEKYLL_VERSION}
+version=3.8
+tag=jekyll-builder-${version}
+
+docker rm -f ${tag} || exit 10
+docker run --name=${tag} -d jekyll/builder:${version} //bin/true || exit 20
+sleep 5s || exit 30
+docker cp -r ./_src/* ${tag}:/srv/jekyll/ || exit 40
+docker exec ${tag} jekyll build || exit 50
+rm -rf _site || exit 60
+mkdir _site || exit 70
+docker cp -r ${tag}:/srv/jekyll/ _site/ || exit 80
+docker rm -f ${tag} || exit 90
+
