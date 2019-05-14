68bcb4edb7d7e21ba0502ee0685855c3f172033b
Miroslav Vaško <vasko@Miroslavs-Mac-mini.local>
2019-05-14 11:06:09 +0200

    Build works

--- build.sh
+++ build.sh
@@ -3,13 +3,23 @@
 version=3.8
 tag=jekyll-builder-${version}
 
-docker rm -f ${tag} || exit 10
-docker run --name=${tag} -d jekyll/builder:${version} //bin/true || exit 20
-sleep 5s || exit 30
-docker cp -r ./_src/* ${tag}:/srv/jekyll/ || exit 40
+docker rm -f ${tag}
+
+echo Starting container ...
+docker run --name=${tag} -d jekyll/builder:${version} tail -f //etc/hosts || exit 20
+
+echo Waiting for 3 seconds ...
+sleep 3s || exit 30
+
+echo Copying files into container ...
+docker cp ./_src/* ${tag}:/srv/jekyll/ || exit 40
+
+echo Building ...
 docker exec ${tag} jekyll build || exit 50
+
+echo Copying site out of the container ...
 rm -rf _site || exit 60
 mkdir _site || exit 70
-docker cp -r ${tag}:/srv/jekyll/ _site/ || exit 80
-docker rm -f ${tag} || exit 90
+docker cp ${tag}:/srv/jekyll/ _site/ || exit 80
 
+docker rm -f ${tag}
